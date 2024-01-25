5ae9ee8a1aeef816543aaecdf594cbf41bc1eb6c
paradoxniteowl <148103497+paradoxniteowl@users.noreply.github.com>
2024-01-25 11:14:44 -0600

    1/25

--- tank.gd
+++ tank.gd
@@ -2,15 +2,15 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const ROTATION_SPEED = 1.5
 
 func _physics_process(delta):
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var move = Input.get_axis("ui_up", "ui_down")
-	if move == "ui_up":
-		velocity.y = move * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-	var rotate = Input.get_axis("ui_left", "ui_right")
-	if rotate
+	# As good practice, you should replace UI actions with custom gameplay actions
+	if Input.is_action_just_pressed("ui_up"):
+		velocity.y = -SPEED
+	elif Input.is_action_just_pressed("ui_down"):
+		velocity.y = SPEED
+	elif Input.is_action_just_pressed("ui_left"):
+		velocity.x = -SPEED
+	elif Input.is_action_just_pressed("ui_right"):
+		velocity.x = SPEED
 	move_and_slide()
