e300a6652be427d7de1b5753a20bc91942122e0c
paradoxniteowl <148103497+paradoxniteowl@users.noreply.github.com>
2024-02-05 11:07:59 -0600

    Fixed collisions and moving

--- tank.gd
+++ tank.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+class_name Player
 
 const SPEED = 300.0
 
@@ -7,10 +8,22 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions
 	if Input.is_action_just_pressed("ui_up"):
 		velocity.y = -SPEED
+		if rotation != 180:
+			rotation = 0 + 180
 	elif Input.is_action_just_pressed("ui_down"):
 		velocity.y = SPEED
+		if rotation != 0:
+			rotation = 0
 	elif Input.is_action_just_pressed("ui_left"):
 		velocity.x = -SPEED
+		if rotation != 90:
+			rotation = 0 + 90
 	elif Input.is_action_just_pressed("ui_right"):
 		velocity.x = SPEED
+		if rotation != 270:
+			rotation = 0 + 270
+	elif Input.is_action_just_released("ui_up") or Input.is_action_just_released("ui_down"):
+		velocity.y = 0
+	elif Input.is_action_just_released("ui_left") or Input.is_action_just_released("ui_right"):
+		velocity.x = 0
 	move_and_slide()
